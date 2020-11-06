118e832d987253c7c7fcd3d7d769b045cf7c3f72
Jerud-Moyer <jerudmoyer@gmail.com>
2020-11-05 16:29:45 -0800

    post route done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,30 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const Fact = require('../lib/models/fact');
 
 describe('portfolio-site-be routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('creates a fact via POST', () => {
+    return request(app)
+      .post('/api/v1/facts')
+      .send({
+        type: 'memory',
+        colorCode: 'blue',
+        text: 'blah blah blah',
+        imageUrl: 'url.com'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          type: 'memory',
+          colorCode: 'blue',
+          text: 'blah blah blah',
+          imageUrl: 'url.com'
+        });
+      });
   });
 });
