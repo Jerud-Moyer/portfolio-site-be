4b5c3b56495c0f0a4db7daf197bc920a14b1b7ed
Jerud-Moyer <jerudmoyer@gmail.com>
2020-11-06 16:16:40 -0800

    get route working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
-const Fact = require('../lib/models/fact');
+const Fact = require('../lib/models/Fact');
 
 describe('portfolio-site-be routes', () => {
   beforeEach(() => {
@@ -26,6 +26,35 @@ describe('portfolio-site-be routes', () => {
           text: 'blah blah blah',
           imageUrl: 'url.com'
         });
+      });
+  });
+
+  it('finds all facts via GET', async() => {
+    const facts = await Promise.all([
+      Fact.insert({
+        type: 'memory',
+        colorCode: 'blue',
+        text: 'blah blah blah',
+        imageUrl: 'url.com'
+      }),
+      Fact.insert({
+        type: 'personal',
+        colorCode: 'green',
+        text: 'blue blue blue',
+        imageUrl: 'p-url.com'
+      }),
+      Fact.insert({
+        type: 'professional',
+        colorCode: 'yellow',
+        text: 'blarg blarg blarg',
+        imageUrl: 'F-url.com'
+      })
+    ]);
+
+    return request(app)
+      .get('/api/v1/facts')
+      .then(res => {
+        expect(res.body).toEqual(expect.arrayContaining(facts));
       });
   });
 });
